d432340eb27595bb001d4452a0847f416324ce4b
Mohamed Amine Boudagga <ma.bdg94@gmail.com>
2024-09-01 08:02:49 +0200

    Setup Redux

--- remotefix2-client/src/index.tsx
+++ remotefix2-client/src/index.tsx
@@ -6,6 +6,8 @@ import "./i18n/i18n";
 import App from "./App";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,9 +15,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </HelmetProvider>
   </React.StrictMode>
 );
