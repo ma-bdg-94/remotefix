a83e35bbb3d604003fb4ce7a2f96f6227eab760a
Mohamed Amine Boudagga <ma.bdg94@gmail.com>
2024-09-01 10:36:36 +0200

    Setup Files Finalizations --version-3

--- remotefix2-client/src/i18n/i18n.ts
+++ remotefix2-client/src/i18n/i18n.ts
@@ -2,12 +2,10 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import translationEN from "./locales/en.json";
 import translationAR from "./locales/ar.json";
-import translationFR from "./locales/fr.json";
 
 const resources = {
   en: { translation: translationEN },
   ar: { translation: translationAR },
-  fr: { translation: translationFR },
 };
 
 const getInitialLanguage = () => {
@@ -15,7 +13,7 @@ const getInitialLanguage = () => {
   if (savedLanguage) {
     return savedLanguage;
   }
-  return ["en", "fr", "ar"].includes(navigator.language?.substring(0, 2))
+  return ["en", "ar"].includes(navigator.language?.substring(0, 2))
     ? navigator.language?.substring(0, 2)
     : "en";
 };
